46a0ca2916e25e4d31e4311c4d0ab2195bacae78
Yesid M <wujibifan.hh@gmail.com>
2024-07-02 23:47:17 -0500

    Data types

--- app/src/main/java/com/notes/javaapp/MainActivity.java
+++ app/src/main/java/com/notes/javaapp/MainActivity.java
@@ -1,24 +1,43 @@
+// Main Class
+
 package com.notes.javaapp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define a variable named TAG to identify the source of the log messages, typically set to the name of the current class.
+//    private static final String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Primitive data type. It is 16 bits long
+        // Can store any valid Unicode character, including letters, digits, symbols, and special characters
+        char gender = 'M';
+        // To avoid errors, make sure that char variables contain only one character
+
+        // Its size depends on the number of bits used to store them
+        int availableProducts = 10;
+
+        // Represents a 32-bit floating-point numeric value
+        // It can represent a wide range of fractional values with a certain degree of accuracy
+        float price = 19.9f;
+        // Float numbers must be followed by an 'f' or 'F' to differentiate them from double
+
+        boolean isAvailable = true;
+
+        String productName = "Product A";
+
+        System.out.println("Gender: " + gender);
+        System.out.println("Available Products: " + availableProducts);
+        System.out.println("Price: " + price);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("Product Name: " + productName);
+
     }
 }
